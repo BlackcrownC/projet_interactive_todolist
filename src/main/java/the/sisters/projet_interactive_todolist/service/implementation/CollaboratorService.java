ccb2a16d19f2994ad747356d3fc055bc2944f0ba
Cédryk <cedrykjutras@hotmail.com>
2021-04-06 11:51:37 -0400

    Services linked with their repositories

--- src/main/java/the/sisters/projet_interactive_todolist/service/implementation/CollaboratorService.java
+++ src/main/java/the/sisters/projet_interactive_todolist/service/implementation/CollaboratorService.java
@@ -1,7 +1,14 @@
 package the.sisters.projet_interactive_todolist.service.implementation;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import the.sisters.projet_interactive_todolist.repository.Interfaces.ICollaboratorRepository;
 import the.sisters.projet_interactive_todolist.service.ICollaboratorService;
 
 public class CollaboratorService implements ICollaboratorService {
+    private final ICollaboratorRepository collaboratorRepository;
+    @Autowired
+    public CollaboratorService(ICollaboratorRepository collaboratorRepository) {
+        this.collaboratorRepository = collaboratorRepository;
+    }
 }
--- src/main/java/the/sisters/projet_interactive_todolist/service/implementation/ProjectService.java
+++ src/main/java/the/sisters/projet_interactive_todolist/service/implementation/ProjectService.java
@@ -1,6 +1,13 @@
 package the.sisters.projet_interactive_todolist.service.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import the.sisters.projet_interactive_todolist.repository.Interfaces.IProjectRepository;
 import the.sisters.projet_interactive_todolist.service.IProjectService;
 
 public class ProjectService implements IProjectService {
+    private final IProjectRepository projectRepository;
+    @Autowired
+    public ProjectService(IProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
 }
