1bb02e7858c444e4c109d9b6f05f547ee490aa51
Cédryk <cedrykjutras@hotmail.com>
2021-04-06 09:31:44 -0400

    Initial commit

---
+++ src/main/java/the/sisters/projet_interactive_todolist/ProjetInteractiveTodolistApplication.java
@@ -0,0 +1,13 @@
+package the.sisters.projet_interactive_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetInteractiveTodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetInteractiveTodolistApplication.class, args);
+    }
+
+}
